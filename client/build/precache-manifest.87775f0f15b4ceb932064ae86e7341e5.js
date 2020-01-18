50192f1651bc79a2c398fef4c48aeb522458b4ae
Austin Graves <austingraves106@aol.com>
2020-01-17 20:50:49 -0600

    Adding in the build folder

---
+++ client/build/precache-manifest.87775f0f15b4ceb932064ae86e7341e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d568c21ac50d79be1a9f192df8e0309a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fc5b76bee0996fa8409",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "28b2df5485ec0823cc70",
+    "url": "/static/css/main.99f15dd6.chunk.css"
+  },
+  {
+    "revision": "2fc5b76bee0996fa8409",
+    "url": "/static/js/2.0d567c5d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.0d567c5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28b2df5485ec0823cc70",
+    "url": "/static/js/main.e61d0492.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
